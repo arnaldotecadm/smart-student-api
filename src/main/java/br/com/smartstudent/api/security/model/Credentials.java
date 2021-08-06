9796e4959a139686ac9b21b6c3b51752dfa94638
Arnaldo C. Bezerra <arnaldotecadm@hotmail.com>
2021-08-06 22:53:07 +0100

    Adicionando a camada de segurança e refatorando o código.

---
+++ src/main/java/br/com/smartstudent/api/security/model/Credentials.java
@@ -0,0 +1,21 @@
+package br.com.smartstudent.api.security.model;
+
+import com.google.firebase.auth.FirebaseToken;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Credentials {
+
+    public enum CredentialType {
+        ID_TOKEN, SESSION
+    }
+
+    private CredentialType type;
+    private FirebaseToken decodedToken;
+    private String idToken;
+    private String session;
+
+}
