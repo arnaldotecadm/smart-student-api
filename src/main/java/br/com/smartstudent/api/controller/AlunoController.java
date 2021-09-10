eec020c38318b1623f3059da747088b7c0c60960
Arnaldo C. Bezerra <arnaldotecadm@hotmail.com>
2021-09-10 16:25:43 +0100

    Add endpoint para recuperar aluno por id de usuario

--- src/main/java/br/com/smartstudent/api/controller/AlunoController.java
+++ src/main/java/br/com/smartstudent/api/controller/AlunoController.java
@@ -4,9 +4,11 @@ import br.com.smartstudent.api.controller.generics.RestBasicController;
 import br.com.smartstudent.api.model.Aluno;
 import br.com.smartstudent.api.service.AlunoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 
 @RestController
@@ -25,5 +27,12 @@ public class AlunoController extends RestBasicController<Aluno> {
     @GetMapping("aluno-por-turma/{turmaId}")
     public List<Aluno> getAlunosPorTurma(@PathVariable("turmaId") String turmaId) throws ExecutionException, InterruptedException {
         return this.service.getAlunosByTurma(turmaId);
+    }
+
+    @GetMapping("aluno-por-uid/{usuarioUid}")
+   public ResponseEntity getAlunoByUsuarioUid(@PathVariable("usuarioUid") String usuarioUid) throws ExecutionException, InterruptedException {
+        Optional<Aluno> byUsuario = this.service.getByUsuario(usuarioUid);
+        Aluno aluno = byUsuario.orElseGet(null);
+        return ResponseEntity.ok(aluno);
     }
 }
